a7db0069fa1fb4f03c36729b1e9137600dd5a51d
Malick <diop-malick@hotmail.fr>
2016-03-21 22:56:24 +0100

    init multi mode maven project

---
+++ electoralSystem-api/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $net.webapp.electoralsystem.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
